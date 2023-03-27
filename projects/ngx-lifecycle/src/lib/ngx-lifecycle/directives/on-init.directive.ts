bda704a9006f13bb7cf09334f182d99ee14bfe09
Sebastian Post <sebastianpost@online.de>
2023-03-27 20:25:33 +0200

    initial commit

--- projects/ngx-lifecycle/src/lib/ngx-lifecycle/directives/on-init.directive.ts
+++ projects/ngx-lifecycle/src/lib/ngx-lifecycle/directives/on-init.directive.ts
@@ -7,6 +7,7 @@ export class OnInitDirective implements OnInit {
   @Output() ngxInit = new EventEmitter<void>();
 
   ngOnInit(): void {
+    this.ngxInit.complete();
     throw new Error('Method not implemented.');
   }
 }
